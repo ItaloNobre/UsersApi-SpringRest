c453f49f3d405165d9ff2e38dba21ecc3b559018
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-21 23:42:38 -0300

    created method findAll, findById, create, update

---
+++ src/main/java/com/italo/simpleApi/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package com.italo.simpleApi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.italo.simpleApi.model.Usuario;
+import com.italo.simpleApi.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    UsuarioService service;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id){
+        Usuario obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    
+    @PostMapping("/")
+    public ResponseEntity<Usuario> create(@RequestBody Usuario obj){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(obj));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario obj){
+       Usuario newObj = service.update(id,obj);
+       return ResponseEntity.ok().body(newObj);
+    }
+}
