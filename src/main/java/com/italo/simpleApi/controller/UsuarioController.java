851815fb67cf7c4d64e2afb87a58b82deba9e735
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-22 00:32:50 -0300

    Created method Delete for Usuario

--- src/main/java/com/italo/simpleApi/controller/UsuarioController.java
+++ src/main/java/com/italo/simpleApi/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package com.italo.simpleApi.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.italo.simpleApi.model.Usuario;
 import com.italo.simpleApi.service.UsuarioService;
@@ -36,11 +39,18 @@ public class UsuarioController {
     
     @PostMapping("/")
     public ResponseEntity<Usuario> create(@RequestBody Usuario obj){
+
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(obj));
     }
     @PutMapping("/{id}")
     public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario obj){
        Usuario newObj = service.update(id,obj);
-       return ResponseEntity.ok().body(newObj);
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+       return ResponseEntity.created(uri).build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
