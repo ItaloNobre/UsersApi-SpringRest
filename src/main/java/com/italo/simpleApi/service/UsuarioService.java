a46c3278e32f42cdbbb26f3c3281f325ff102291
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-21 23:51:08 -0300

    create methods findById, create, findAll, update in UserService

---
+++ src/main/java/com/italo/simpleApi/service/UsuarioService.java
@@ -0,0 +1,41 @@
+package com.italo.simpleApi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.italo.simpleApi.exceptions.ObjectNotFoundException;
+import com.italo.simpleApi.model.Usuario;
+import com.italo.simpleApi.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository repository;
+
+    public Usuario findById(Long id){
+        Optional<Usuario> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! id: " 
+            +id+" Tipo: "+ Usuario.class.getName()));
+    }
+
+    public Usuario create(Usuario obj) {
+        return repository.save(obj);
+    }
+
+    public List<Usuario> findAll() {
+        return repository.findAll();
+    }
+    
+    public Usuario update(Long id, Usuario obj){
+        Usuario newObj = findById(id);
+        newObj.setName(obj.getName());
+        newObj.setLogin(obj.getLogin());
+        newObj.setSenha(obj.getSenha());
+        return repository.save(newObj);
+    }
+
+}
