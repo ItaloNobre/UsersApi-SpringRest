fc9b4355e1406b8c2735f5dfb8ad2355c0987646
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-21 23:40:10 -0300

    exception handling

---
+++ src/main/java/com/italo/simpleApi/controller/exceptions/StandardError.java
@@ -0,0 +1,17 @@
+package com.italo.simpleApi.controller.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class StandardError {
+    private Integer status;
+    private Long timestamp;
+    private String message;
+
+}
